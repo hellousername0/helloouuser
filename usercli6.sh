d9e0b8eb1c63154a1e8a98981471e1a9256a11af
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-17 09:15:40 +0700

    Create usercli6.sh

---
+++ usercli6.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk6.sh 
+chmod +x /home/ubuntu/uservrsk6.sh
+sleep 6
+sudo /home/ubuntu/uservrsk6.sh
