9e08af17f18e77e504848acb95bd9477b64982a0
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-17 09:41:25 +0700

    Update usercli6.sh

--- usercli6.sh
+++ usercli6.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk6.sh 
+wget -P /home/ubuntu https://raw.githubusercontent.com/hellousername0/helloouuser/main/uservrsk6.sh 
 chmod +x /home/ubuntu/uservrsk6.sh
 sleep 6
 sudo /home/ubuntu/uservrsk6.sh
