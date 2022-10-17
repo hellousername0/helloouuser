8e7492ec1b32443107e90a53a9cfb5ec074b9429
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-17 09:16:07 +0700

    Create usercli2.sh

---
+++ usercli2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk2.sh 
+chmod +x /home/ubuntu/uservrsk2.sh
+sleep 6
+sudo /home/ubuntu/uservrsk2.sh
