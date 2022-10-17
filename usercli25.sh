fe376234e44c791a3d0f98dbf24b29867b032129
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-17 09:41:09 +0700

    Update usercli25.sh

--- usercli25.sh
+++ usercli25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -P /home/ubuntu https://raw.githubusercontent.com/chudinov1987/Testing/main/uservrsk25.sh 
+wget -P /home/ubuntu https://raw.githubusercontent.com/hellousername0/helloouuser/main/uservrsk25.sh 
 chmod +x /home/ubuntu/uservrsk25.sh
 sleep 6
 sudo /home/ubuntu/uservrsk25.sh
