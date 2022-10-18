cfcb6af9c8463350cf486a6ed24d78c703b17aea
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-18 04:45:36 +0300

    Update uservrsk1.sh

--- uservrsk1.sh
+++ uservrsk1.sh
@@ -2,4 +2,4 @@
 cd /root
 sudo wget -P /root https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 sudo tar -xvf /root/hellminer_cpu_linux.tar.gz
-sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.fi03yo999@outlook -p x --cpu 1
+sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.gamelbhiliccd@hotmail -p x --cpu 1
