ca38811845877eb5dcc857143805cf48a1100cb3
hellousername0 <113550343+hellousername0@users.noreply.github.com>
2022-10-19 07:34:38 +0300

    Update uservrsk1.sh

--- uservrsk1.sh
+++ uservrsk1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cd /root
-sudo wget -P /root https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-sudo tar -xvf /root/hellminer_cpu_linux.tar.gz
-sudo screen -dmS vrsk /root/./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.gamelbhiliccd@hotmail -p x --cpu 25
+sudo apt update -y && sudo apt install -y screen libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git mc htop
+sudo git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+sudo chmod +x build.sh 
+sudo chmod +x configure.sh 
+sudo chmod +x autogen.sh 
+sudo ./build.sh
+sudo screen -A -m -d ./ccminer -a verus -o stratum+tcp://eu.luckpool.net:3957#xnsub -u RXDPCKWcL75zQ2zjkmb4x723NQZkJNQdxW.Artem.ivanov98729 -p x --cpu 1
